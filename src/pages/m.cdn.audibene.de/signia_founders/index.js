5a6565c8c82796332745e45690d3de35fb112656
Alex Ranc <alex.ranc@hear.com>
2020-05-07 16:36:34 -0400

    create domain directories

---
+++ src/pages/m.cdn.audibene.de/signia_founders/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../../components/layout"
+import Image from "../../../components/image"
+import SEO from "../../../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Signia Founders" />
+    <h1>Signia Founders audibene.de</h1>
+  </Layout>
+)
+
+export default IndexPage
